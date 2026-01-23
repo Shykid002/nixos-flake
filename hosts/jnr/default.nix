d0f3c23b295fd53a2bff0485aab8c6d573279c11
Shykid002 <121339525+Shykid002@users.noreply.github.com>
2026-01-23 14:59:23 +0100

    Fix ACPI service configuration syntax
    Added ;

--- hosts/jnr/default.nix
+++ hosts/jnr/default.nix
@@ -19,7 +19,7 @@
   #boot.initrd.kernelModules = [ "i915" ];
   boot.kernelModules = [ ];
   hardware.firmware = [ "intel-ucode" ];
-  services.acpi.enable = true
+  services.acpi.enable = true;
 
 
   networking.hostName = "jnr";
