365bc72feb16a71f07d20f4d3b17b9823ebb7ba9
Shykid002 <121339525+Shykid002@users.noreply.github.com>
2026-01-23 15:29:49 +0100

    Remove custom overlays from nixpkgs configuration
    Removed custom overlays for the 'st' package.

--- system/default.nix
+++ system/default.nix
@@ -34,22 +34,7 @@
   programs.hyprland.enable = true;
 
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.overlays = [
-    inputs.nur.overlays.default
-    (self: super: {
-      st = super.st.overrideAttrs (old: {
-        src = pkgs.fetchFromGitHub {
-          owner = " samiulbasirfahim ";
-          repo = "st";
-          rev = "926ad9f29b179bd5fe16f4e07dbdcec83def6785";
-          hash = "sha256-hwX1XhSX8KhXHROuKLEMzdYtd86/aMsetIycPLN0l2I=";
-        };
-        buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.harfbuzz ];
-        nativeBuildInputs = (old.nativeBuildInputs or [ ])
-          ++ [ pkgs.pkg-config ];
-      });
-    })
-  ];
+  
 
   nix = {
     gc = {
